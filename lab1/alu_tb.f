6ea2f243406730c81a0ab3bbd2b7196768135d4d
wojtek2407 <wojtek2407972@gmail.com>
2021-10-14 18:28:13 +0200

    lab1

--- lab1/alu_tb.f
+++ lab1/alu_tb.f
@@ -1,3 +1,4 @@
 -v mtm_Alu.vp
+-v crc4.v
 
 alu_tb.sv
--- lab1/alu_tb.sv
+++ lab1/alu_tb.sv
@@ -1,10 +1,14 @@
 `timescale 1ns/1ps
 
+
 module alu_tb();
 	
-	wire clk;
-	wire rst_n;
-	wire sin;
+	reg clk;
+	reg rst_n;
+	reg sin;
+	
+	always #10 clk = ~clk;
+	
 	wire sout;
 
 	mtm_Alu u_mtm_Alu (
@@ -12,11 +16,53 @@ module alu_tb();
 		.rst_n(rst_n), //synchronous reset active low
 		.sin  (sin), //serial data input
 		.sout (sout) //serial data output
-		);
+        );
 	
 	initial begin
+		
+        clk = 1'b0;
+        rst_n = 1'b0;
+        sin = 1'b1;
+        #1000;
+        rst_n = 1'b1;
+        
+        #1000;
+        
+        test1;
+        
+        #100000;
+
 		$display("PASS");
+		$finish();
 	end
 	
+	task send_data(input reg [7:0] data);
+        integer i;
+		begin
+			repeat(2) @(negedge clk) sin = 1'b0;
+			for (i = 0; i < 8; i = i + 1)
+                @(negedge clk) sin = data[7-i];
+			@(negedge clk) sin = 1'b1;
+		end
+	endtask
+	
+    task send_cmd(input reg [7:0] cmd);
+        integer i;
+        begin
+            @(negedge clk) sin = 1'b0;
+            @(negedge clk) sin = 1'b1;
+            for (i = 0; i < 8; i = i + 1)
+                @(negedge clk) sin = cmd[7-i];
+            @(negedge clk) sin = 1'b1;
+        end
+    endtask
+
+	task test1;
+		begin
+			repeat(7) send_data(0);
+            send_data(1);
+            send_cmd(8'haa);
+		end
+	endtask
 	
 endmodule
